cd3122375000cdb93371744001c127a103e04c29
Marcos Lucas <marcosvaldeni@gmail.com>
2020-07-13 14:28:53 +0100

    Styling Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,92 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore respositorios no </Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositories on Github</Title>
+
+      <Form action="">
+        <input placeholder="Inform the repository's name" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://vincentpaone.files.wordpress.com/2011/10/lost-locke.jpg"
+            alt="John Locke"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://vincentpaone.files.wordpress.com/2011/10/lost-locke.jpg"
+            alt="John Locke"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://vincentpaone.files.wordpress.com/2011/10/lost-locke.jpg"
+            alt="John Locke"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://vincentpaone.files.wordpress.com/2011/10/lost-locke.jpg"
+            alt="John Locke"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://vincentpaone.files.wordpress.com/2011/10/lost-locke.jpg"
+            alt="John Locke"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://vincentpaone.files.wordpress.com/2011/10/lost-locke.jpg"
+            alt="John Locke"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
